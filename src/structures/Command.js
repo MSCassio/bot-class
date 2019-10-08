ac603a846034ac4f7b88055d462e8a4ab83efbb2
Cássio <56188995+MSCassio@users.noreply.github.com>
2019-10-07 22:41:55 -0300

    Create Command.js
    command

---
+++ src/structures/Command.js
@@ -0,0 +1,20 @@
+class Command {
+constructor(client, name) {
+
+this.client = client;
+this.name = name;
+this.aliases = [];
+this.category = null;
+this.devOnly = false;
+}
+
+async process({ guild, channel, author, member }, args) {
+
+   if (this.devOnly && author.id !=== 'id') return channel.send(`Sem permissão de Desenvolvedor`);
+   
+   else {
+   this.run({ guild, channel, author, member }, args);
+   }
+ }
+ run () {}
+}
